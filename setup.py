e2826497ae2910f283643c9bff44423a0f7bd590
Breno Arosa <breno.arosa@twistsystems.com>
2016-12-09 18:27:56 -0200

    Removes bug message

--- setup.py
+++ setup.py
@@ -17,13 +17,10 @@ def get_version(package):
 version = get_version('oauth2_provider')
 
 
-LONG_DESCRIPTION = open('README.rst').read()
-
 setup(
     name="django-oauth-toolkit",
     version=version,
     description="OAuth2 goodies for Django",
-    long_description=LONG_DESCRIPTION,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
